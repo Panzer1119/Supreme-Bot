1283351061adf5fe22874a744a224a260ec34350
Paul Hagedorn <panzer1119@gmail.com>
2017-07-20 02:54:21 +0200

    Added private flag for HelpCommand

--- src/de/panzercraft/bot/supreme/commands/impl/HelpCommand.java
+++ src/de/panzercraft/bot/supreme/commands/impl/HelpCommand.java
@@ -2,6 +2,7 @@ package de.panzercraft.bot.supreme.commands.impl;
 
 import de.panzercraft.bot.supreme.commands.Command;
 import de.panzercraft.bot.supreme.commands.CommandHandler;
+import de.panzercraft.bot.supreme.commands.arguments.ArgumentConsumeType;
 import de.panzercraft.bot.supreme.commands.arguments.ArgumentList;
 import de.panzercraft.bot.supreme.permission.PermissionRoleFilter;
 import de.panzercraft.bot.supreme.util.Standard;
@@ -30,11 +31,12 @@ public class HelpCommand implements Command {
     @Override
     public void action(String invoke, ArgumentList arguments, MessageReceivedEvent event) { //TODO Das mit Seiten und so machen, wie bei der Musik Playlist....
         if (arguments != null && arguments.isSize(1, -1)) {
+            final boolean sendPrivate = arguments.isConsumed(Standard.ARGUMENT_PRIVATE, ArgumentConsumeType.CONSUME_ALL_IGNORE_CASE);
             while (arguments.hasArguments()) {
                 final String command_invoke = arguments.consumeFirst();
                 final Command command = CommandHandler.getCommandByInvoke(command_invoke);
                 if (command != null) {
-                    CommandHandler.sendHelpMessage(event, command, false); //TODO sendPrivate
+                    CommandHandler.sendHelpMessage(event, command, sendPrivate);
                 } else {
                     final Message message = event.getTextChannel().sendMessageFormat(":warning: Sorry %s, the command \"%s\" wasn't found!", event.getAuthor().getAsMention(), command_invoke).complete();
                     final long delay = Standard.getAutoDeleteCommandNotFoundMessageDelay();
@@ -56,7 +58,7 @@ public class HelpCommand implements Command {
     @Override
     public EmbedBuilder getHelp(EmbedBuilder builder) {
         for (String invoke : getInvokes()) {
-            builder.addField(invoke + " [Command 1] [Command 2] [Command 3]...", "Returns the help for the given commands.", false);
+            builder.addField(String.format("%s [%s] [Command 1] [Command 2] [Command 3]...", invoke, Standard.ARGUMENT_PRIVATE.getCompleteArgument(0)), String.format("Returns the help for the given commands. And with the flag \"%s\" the help will be send in the private chat.", Standard.ARGUMENT_PRIVATE.getCompleteArgument(0)), false);
         }
         return builder;
     }
