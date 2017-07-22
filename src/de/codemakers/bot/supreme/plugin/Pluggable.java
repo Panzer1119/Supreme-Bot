aa94dc190c6b08c8ba36bdc2f88c8b0ff32d2637
Paul Hagedorn <panzer1119@gmail.com>
2017-07-22 19:41:02 +0200

    Implemented first Plugin stuff

---
+++ src/de/codemakers/bot/supreme/plugin/Pluggable.java
@@ -0,0 +1,10 @@
+package de.codemakers.bot.supreme.plugin;
+
+/**
+ * Pluggable
+ *
+ * @author Panzer1119
+ */
+public interface Pluggable {
+
+}
---
+++ src/de/codemakers/bot/supreme/plugin/PluggableManager.java
@@ -0,0 +1,10 @@
+package de.codemakers.bot.supreme.plugin;
+
+/**
+ * PluggableManager
+ *
+ * @author Panzer1119
+ */
+public interface PluggableManager {
+
+}
