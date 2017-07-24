a30095f59ee0269048890c997058b7f11e7a8eac
Paul Hagedorn <panzer1119@gmail.com>
2017-07-24 03:22:04 +0200

    Moved Invoker and added an Invokeable Interface

---
+++ src/de/codemakers/bot/supreme/commands/invoking/Invokeable.java
@@ -0,0 +1,12 @@
+package de.codemakers.bot.supreme.commands.invoking;
+
+/**
+ * Invokeable
+ *
+ * @author Panzer1119
+ */
+public interface Invokeable {
+
+    public Invokeable getInvokeable();
+    
+}
