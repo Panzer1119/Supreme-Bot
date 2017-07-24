e49bf56a3493e9097283e43ac107281de9170ed9
Paul Hagedorn <panzer1119@gmail.com>
2017-07-24 03:15:43 +0200

    Changed text when someone calls System.exit()

--- src/de/codemakers/bot/supreme/core/SupremeBot.java
+++ src/de/codemakers/bot/supreme/core/SupremeBot.java
@@ -246,7 +246,7 @@ public class SupremeBot {
                 return;
             }
             if (permission.getName().equals(EXITVM) || permission.getName().equalsIgnoreCase(EXITVM) || permission.getName().startsWith(EXITVM)) {
-                throw new ExitTrappedException("SOMEONE CALLED \"System.exit();\"!!!");
+                throw new ExitTrappedException("!!WARNING SOMEONE WANTED TO EXIT THE SYSTEM!!!");
             }
         }
     };
