ae537d7d379c77cb7ed5066b6e6e6a453137bc6f
Unknown <panzer1119@gmail.com>
2017-10-06 15:13:03 +0200

    Fixed AdvancedFile Bug

--- Supreme-Bot-Core/src/de/codemakers/bot/supreme/util/AdvancedFile.java
+++ Supreme-Bot-Core/src/de/codemakers/bot/supreme/util/AdvancedFile.java
@@ -122,6 +122,7 @@ public class AdvancedFile {
     }
 
     public final AdvancedFile copyFrom(AdvancedFile advancedFile) {
+        this.paths.clear();
         this.file = advancedFile.file;
         this.folder = advancedFile.folder;
         this.path = advancedFile.path;
@@ -218,6 +219,7 @@ public class AdvancedFile {
         paths_new.addAll(this.paths);
         this.paths.clear();
         this.paths.addAll(paths_new);
+        paths_new.clear();
         return this;
     }
 
