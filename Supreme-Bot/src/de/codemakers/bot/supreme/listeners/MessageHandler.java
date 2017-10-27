3cc3dead4fd15fcc253c36fd316642e726314f6d
Unknown <panzer1119@gmail.com>
2017-10-27 18:44:49 +0200

    Added command recognizing for updated messages

--- Supreme-Bot/src/de/codemakers/bot/supreme/listeners/MessageHandler.java
+++ Supreme-Bot/src/de/codemakers/bot/supreme/listeners/MessageHandler.java
@@ -34,62 +34,19 @@ public class MessageHandler extends ListenerAdapter {
     private static final boolean DEBUG = false;
 
     @Override
-    public final void onMessageReceived(MessageReceivedEvent event_received) {
-        if (event_received == null || event_received.getAuthor().getIdLong() == Standard.getSelfUser().getIdLong()) {
+    public final void onMessageReceived(MessageReceivedEvent event) {
+        if (event == null || event.getAuthor().getIdLong() == Standard.getSelfUser().getIdLong()) {
             return;
         }
-        Updater.submit(() -> {
-            try {
-                final MessageEvent event = new DefaultMessageEvent(event_received.getJDA(), event_received.getResponseNumber(), event_received.getMessage());
-                final String content_raw = event.getMessage().getRawContent().trim();
-                final String content = event.getMessage().getContent().trim();
-                final Guild guild = event.getGuild();
-                final CommandType commandType = CommandType.getCommandType(content, content_raw, event);
-                if (commandType.isCommand()) {
-                    CommandHandler.handleCommand(CommandParser.parser(commandType, content, content_raw, event));
-                } else if (content_raw.contains(Standard.getSelfUser().getAsMention()) || content_raw.contains(Standard.getSelfMemberByGuild(guild).getAsMention())) {
-                    final Emote reaction_emote = Config.CONFIG.getGuildReactionOnMention(guild);
-                    if (reaction_emote != null) {
-                        event.getMessage().addReaction(reaction_emote).queue();
-                    } else {
-                        final String reaction = Config.CONFIG.getGuildReactionOnMention(guild.getIdLong());
-                        if (reaction != null) {
-                            event.getMessage().addReaction(reaction).queue();
-                        }
-                    }
-                }
-                final Object[] output = ListenerManager.fireListeners(MessageListener.class, CommandHandler.ADMIN_PREDICATE, new Object[]{event, MessageType.RECEIVED});
-                String embed_messages = event.getMessage().getEmbeds().stream().map((messageEmbed) -> Util.embedMessageToString(messageEmbed)).collect(Collectors.joining(Standard.NEW_LINE_DISCORD));
-                if (!embed_messages.isEmpty()) {
-                    embed_messages = Standard.NEW_LINE_DISCORD + embed_messages;
-                }
-                final List<Attachment> attachments = event.getMessage().getAttachments();
-                if (attachments == null || attachments.isEmpty()) {
-                    System.out.println(String.format("[Used %d time%s] [%s] [%s] %s: %s%s", output.length, (output.length == 1 ? "" : "s"), (guild != null ? guild.getName() : "PRIVATE"), event.getMessageChannel().getName(), event.getAuthor().getName(), content, embed_messages));
-                } else {
-                    String text = "";
-                    for (Attachment attachment : attachments) {
-                        text += Standard.NEW_LINE_DISCORD;
-                        if (attachment.isImage()) {
-                            text += String.format("IMAGE: \"%s\" (ID: %s) (PROXYURL: %s) (W: %d, H: %d)", attachment.getFileName(), attachment.getId(), attachment.getProxyUrl(), attachment.getWidth(), attachment.getHeight());
-                        } else {
-                            text += String.format("FILE: \"%s\" (ID: %s) (URL: %s)", attachment.getFileName(), attachment.getId(), attachment.getUrl());
-                        }
-                    }
-                    System.out.println(String.format("[Used %d time%s] [%s] [%s] %s: %s%s%s", output.length, (output.length == 1 ? "" : "s"), event.getGuild().getName(), event.getMessageChannel().getName(), event.getAuthor().getName(), content, text, embed_messages));
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
+        onMessage(event, new DefaultMessageEvent(event.getJDA(), event.getResponseNumber(), event.getMessage()), true);
     }
 
     @Override
     public final void onMessageUpdate(MessageUpdateEvent event) {
-        final Object[] output = ListenerManager.fireListeners(MessageListener.class, CommandHandler.ADMIN_PREDICATE, new Object[]{event, MessageType.UPDATED});
-        if (DEBUG && output.length > 0) {
-            System.out.println(String.format("%d plugin%s used this message event: %s", output.length, (output.length == 1 ? "" : "s"), event));
+        if (event == null || event.getAuthor().getIdLong() == Standard.getSelfUser().getIdLong()) {
+            return;
         }
+        onMessage(event, new DefaultMessageEvent(event.getJDA(), event.getResponseNumber(), event.getMessage()), false);
     }
 
     @Override
@@ -138,6 +95,52 @@ public class MessageHandler extends ListenerAdapter {
         if (DEBUG && output.length > 0) {
             System.out.println(String.format("%d plugin%s used this message event: %s", output.length, (output.length == 1 ? "" : "s"), event));
         }
+    }
+
+    private final void onMessage(final Object event_original, final MessageEvent event, boolean newMessage) {
+        Updater.submit(() -> {
+            try {
+                final String content_raw = event.getMessage().getRawContent().trim();
+                final String content = event.getMessage().getContent().trim();
+                final Guild guild = event.getGuild();
+                final CommandType commandType = CommandType.getCommandType(content, content_raw, event);
+                if (commandType.isCommand()) {
+                    CommandHandler.handleCommand(CommandParser.parser(commandType, content, content_raw, event));
+                } else if (content_raw.contains(Standard.getSelfUser().getAsMention()) || content_raw.contains(Standard.getSelfMemberByGuild(guild).getAsMention())) {
+                    final Emote reaction_emote = Config.CONFIG.getGuildReactionOnMention(guild);
+                    if (reaction_emote != null) {
+                        event.getMessage().addReaction(reaction_emote).queue();
+                    } else {
+                        final String reaction = Config.CONFIG.getGuildReactionOnMention(guild.getIdLong());
+                        if (reaction != null) {
+                            event.getMessage().addReaction(reaction).queue();
+                        }
+                    }
+                }
+                final Object[] output = ListenerManager.fireListeners(MessageListener.class, CommandHandler.ADMIN_PREDICATE, new Object[]{newMessage ? event : event_original, newMessage ? MessageType.RECEIVED : MessageType.UPDATED});
+                String embed_messages = event.getMessage().getEmbeds().stream().map((messageEmbed) -> Util.embedMessageToString(messageEmbed)).collect(Collectors.joining(Standard.NEW_LINE_DISCORD));
+                if (!embed_messages.isEmpty()) {
+                    embed_messages = Standard.NEW_LINE_DISCORD + embed_messages;
+                }
+                final List<Attachment> attachments = event.getMessage().getAttachments();
+                if (attachments == null || attachments.isEmpty()) {
+                    System.out.println(String.format("[Used %d time%s] [%s] [%s] %s: %s%s", output.length, (output.length == 1 ? "" : "s"), (guild != null ? guild.getName() : "PRIVATE"), event.getMessageChannel().getName(), event.getAuthor().getName(), content, embed_messages));
+                } else {
+                    String text = "";
+                    for (Attachment attachment : attachments) {
+                        text += Standard.NEW_LINE_DISCORD;
+                        if (attachment.isImage()) {
+                            text += String.format("IMAGE: \"%s\" (ID: %s) (PROXYURL: %s) (W: %d, H: %d)", attachment.getFileName(), attachment.getId(), attachment.getProxyUrl(), attachment.getWidth(), attachment.getHeight());
+                        } else {
+                            text += String.format("FILE: \"%s\" (ID: %s) (URL: %s)", attachment.getFileName(), attachment.getId(), attachment.getUrl());
+                        }
+                    }
+                    System.out.println(String.format("[Used %d time%s] [%s] [%s] %s: %s%s%s", output.length, (output.length == 1 ? "" : "s"), event.getGuild().getName(), event.getMessageChannel().getName(), event.getAuthor().getName(), content, text, embed_messages));
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 
 }
