206bef8d682332dfded0086f8d79e08df1c8bd13
Unknown <panzer1119@gmail.com>
2017-11-12 14:23:18 +0100

    Changed somne startup things

--- Supreme-Bot/src/de/codemakers/bot/supreme/listeners/MessageHandler.java
+++ Supreme-Bot/src/de/codemakers/bot/supreme/listeners/MessageHandler.java
@@ -34,9 +34,9 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
  * @author Panzer1119
  */
 public class MessageHandler extends ListenerAdapter {
-    
+
     private static final boolean DEBUG = false;
-    
+
     static {
         Updater.addUpdateable(new Updateable() {
             @Override
@@ -44,14 +44,14 @@ public class MessageHandler extends ListenerAdapter {
                 ReactionListener.update(Instant.now(), false);
                 return 5_000;
             }
-            
+
             @Override
             public void delete() {
                 ReactionListener.update(Instant.now(), true);
             }
         });
     }
-    
+
     @Override
     public final void onMessageReceived(MessageReceivedEvent event) {
         if (event == null || event.getAuthor().getIdLong() == Standard.getSelfUser().getIdLong()) {
@@ -59,7 +59,7 @@ public class MessageHandler extends ListenerAdapter {
         }
         onMessage(event, new DefaultMessageEvent(event.getJDA(), event.getResponseNumber(), event.getMessage()), true);
     }
-    
+
     @Override
     public final void onMessageUpdate(MessageUpdateEvent event) {
         if (event == null || event.getAuthor().getIdLong() == Standard.getSelfUser().getIdLong()) {
@@ -67,7 +67,7 @@ public class MessageHandler extends ListenerAdapter {
         }
         onMessage(event, new DefaultMessageEvent(event.getJDA(), event.getResponseNumber(), event.getMessage()), false);
     }
-    
+
     @Override
     public final void onMessageDelete(MessageDeleteEvent event) {
         final Object[] output = ListenerManager.fireListeners(MessageListener.class, CommandHandler.ADMIN_PREDICATE, new Object[]{event, MessageType.DELETED});
@@ -75,7 +75,7 @@ public class MessageHandler extends ListenerAdapter {
             System.out.println(String.format("%d plugin%s used this message event: %s", output.length, (output.length == 1 ? "" : "s"), event));
         }
     }
-    
+
     @Override
     public final void onMessageBulkDelete(MessageBulkDeleteEvent event) {
         final Object[] output = ListenerManager.fireListeners(MessageListener.class, CommandHandler.ADMIN_PREDICATE, new Object[]{event, MessageType.BULK_DELETED});
@@ -83,7 +83,7 @@ public class MessageHandler extends ListenerAdapter {
             System.out.println(String.format("%d plugin%s used this message event: %s", output.length, (output.length == 1 ? "" : "s"), event));
         }
     }
-    
+
     @Override
     public final void onMessageEmbed(MessageEmbedEvent event) {
         final Object[] output = ListenerManager.fireListeners(MessageListener.class, CommandHandler.ADMIN_PREDICATE, new Object[]{event, MessageType.EMBEDED});
@@ -91,12 +91,12 @@ public class MessageHandler extends ListenerAdapter {
             System.out.println(String.format("%d plugin%s used this message event: %s", output.length, (output.length == 1 ? "" : "s"), event));
         }
     }
-    
+
     @Override
     public final void onGenericMessageReaction(GenericMessageReactionEvent event) {
         ReactionListener.handle(event);
     }
-    
+
     @Override
     public final void onMessageReactionAdd(MessageReactionAddEvent event) {
         final Object[] output = ListenerManager.fireListeners(MessageListener.class, CommandHandler.ADMIN_PREDICATE, new Object[]{event, MessageType.REACTION_ADDED});
@@ -104,7 +104,7 @@ public class MessageHandler extends ListenerAdapter {
             System.out.println(String.format("%d plugin%s used this message event: %s", output.length, (output.length == 1 ? "" : "s"), event));
         }
     }
-    
+
     @Override
     public final void onMessageReactionRemove(MessageReactionRemoveEvent event) {
         final Object[] output = ListenerManager.fireListeners(MessageListener.class, CommandHandler.ADMIN_PREDICATE, new Object[]{event, MessageType.REACTION_REMOVED});
@@ -112,7 +112,7 @@ public class MessageHandler extends ListenerAdapter {
             System.out.println(String.format("%d plugin%s used this message event: %s", output.length, (output.length == 1 ? "" : "s"), event));
         }
     }
-    
+
     @Override
     public final void onMessageReactionRemoveAll(MessageReactionRemoveAllEvent event) {
         final Object[] output = ListenerManager.fireListeners(MessageListener.class, CommandHandler.ADMIN_PREDICATE, new Object[]{event, MessageType.REACTION_REMOVED_ALL});
@@ -120,7 +120,7 @@ public class MessageHandler extends ListenerAdapter {
             System.out.println(String.format("%d plugin%s used this message event: %s", output.length, (output.length == 1 ? "" : "s"), event));
         }
     }
-    
+
     private final void onMessage(final Object event_original, final MessageEvent event, boolean newMessage) {
         Updater.submit(() -> {
             try {
@@ -148,7 +148,7 @@ public class MessageHandler extends ListenerAdapter {
                 }
                 final List<Attachment> attachments = event.getMessage().getAttachments();
                 if (attachments == null || attachments.isEmpty()) {
-                    System.out.println(String.format("[Used %d time%s] [%s] [%s] %s: %s%s", output.length, (output.length == 1 ? "" : "s"), (guild != null ? guild.getName() : "PRIVATE"), event.getMessageChannel().getName(), event.getAuthor().getName(), content, embed_messages));
+                    System.out.println(String.format("[Used %d time%s] [%s] [%s] %s: %s%s", output.length, (output.length == 1 ? "" : "s"), (guild != null ? guild.getName() : "PRIVATE"), event.getMessageChannel().getName(), Standard.getCompleteName(event.getAuthor(), true), content, embed_messages));
                 } else {
                     String text = "";
                     for (Attachment attachment : attachments) {
@@ -159,12 +159,12 @@ public class MessageHandler extends ListenerAdapter {
                             text += String.format("FILE: \"%s\" (ID: %s) (URL: %s)", attachment.getFileName(), attachment.getId(), attachment.getUrl());
                         }
                     }
-                    System.out.println(String.format("[Used %d time%s] [%s] [%s] %s: %s%s%s", output.length, (output.length == 1 ? "" : "s"), event.getGuild().getName(), event.getMessageChannel().getName(), event.getAuthor().getName(), content, text, embed_messages));
+                    System.out.println(String.format("[Used %d time%s] [%s] [%s] %s: %s%s%s", output.length, (output.length == 1 ? "" : "s"), event.getGuild().getName(), event.getMessageChannel().getName(), Standard.getCompleteName(event.getAuthor(), true), content, text, embed_messages));
                 }
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         });
     }
-    
+
 }
--- Supreme-Bot/src/de/codemakers/bot/supreme/listeners/ReadyListener.java
+++ Supreme-Bot/src/de/codemakers/bot/supreme/listeners/ReadyListener.java
@@ -2,8 +2,11 @@ package de.codemakers.bot.supreme.listeners;
 
 import de.codemakers.bot.supreme.entities.AdvancedGuild;
 import de.codemakers.bot.supreme.sql.MySQL;
+import de.codemakers.bot.supreme.util.AdvancedFile;
 import de.codemakers.bot.supreme.util.Standard;
+import java.time.LocalDateTime;
 import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.ReconnectedEvent;
 import net.dv8tion.jda.core.events.ResumedEvent;
@@ -16,12 +19,16 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
  */
 public class ReadyListener extends ListenerAdapter {
 
+    private static final AdvancedFile SERVERS_FOLDER = new AdvancedFile(Standard.STANDARD_DATA_FOLDER, "servers");
+
+    static {
+        SERVERS_FOLDER.createAdvancedFile();
+    }
+
     @Override
     public final void onReady(ReadyEvent event) {
         final JDA jda = event.getJDA();
-        final StringBuilder out = new StringBuilder();
-        loadGuilds(jda, out, false);
-        System.out.print(out.toString());
+        loadGuilds(jda);
     }
 
     @Override
@@ -37,22 +44,41 @@ public class ReadyListener extends ListenerAdapter {
         Standard.clearGuilds();
         final StringBuilder out = new StringBuilder();
         out.append("Bot just reconnected and needs to reload everyting").append(Standard.NEW_LINE_DISCORD);
-        loadGuilds(jda, out, false);
         System.out.print(out.toString());
+        loadGuilds(jda);
     }
 
-    private final void loadGuilds(JDA jda, StringBuilder out, boolean withRoles) {
-        out.append("This Bot is running on this Guilds:").append(Standard.NEW_LINE_DISCORD);
-        jda.getGuilds().stream().forEach((guild) -> {
-            final AdvancedGuild advancedGuild = Standard.getAdvancedGuild(guild);
-            out.append(String.format("%s (ID: %s)", guild.getName(), guild.getId())).append(Standard.NEW_LINE_DISCORD);
-            if (withRoles) {
-                guild.getRoles().stream().forEach((role) -> {
-                    out.append(Standard.TAB).append(String.format("%s (ID: %s) (%s)", role.getName(), role.getId(), role.getAsMention())).append(Standard.NEW_LINE_DISCORD);
-                });
-                out.append(Standard.NEW_LINE_DISCORD);
-            }
-        });
+    private final void loadGuilds(JDA jda) {
+        try {
+            final LocalDateTime now = LocalDateTime.now(Standard.getZoneId());
+            final StringBuilder out = new StringBuilder();
+            out.append("This Bot is running on this Guilds:").append(Standard.NEW_LINE);
+            jda.getGuilds().stream().forEach((guild) -> {
+                try {
+                    final AdvancedGuild advancedGuild = Standard.getAdvancedGuild(guild);
+                    out.append(String.format("%s (ID: %s)", guild.getName(), guild.getId())).append(Standard.NEW_LINE);
+                    out.append("Invites:").append(Standard.NEW_LINE);
+                    try {
+                        guild.getInvites().complete().stream().forEach((invite) -> out.append(Standard.TAB).append(String.format("Code: %s, URL: %s, Uses: %d of %d, Max Age: %d, Inviter: %s, isExpanded: %b, isTemporary: %b, Creation Time: %s", invite.getCode(), invite.getURL(), invite.getUses(), invite.getMaxUses(), invite.getMaxAge(), Standard.getCompleteName(invite.getInviter(), true), invite.isExpanded(), invite.isTemporary(), invite.getCreationTime().atZoneSameInstant(Standard.getZoneId()).format(Standard.STANDARD_DATE_TIME_FORMATTER))).append(Standard.NEW_LINE));
+                    } catch (Exception ex) {
+                    }
+                    out.append("Roles:").append(Standard.NEW_LINE);
+                    try {
+                        guild.getRoles().stream().forEach((role) -> out.append(Standard.TAB).append(String.format("%s (ID: %s) (%s)", role.getName(), role.getId(), role.getAsMention())).append(Standard.NEW_LINE));
+                    } catch (Exception ex) {
+                    }
+                    out.append(Standard.NEW_LINE);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+            System.out.println(out.toString());
+            final AdvancedFile file = new AdvancedFile(SERVERS_FOLDER, String.format("servers_%s.txt", now.format(Standard.STANDARD_DATE_TIME_FILE_FORMATTER)));
+            Standard.addToFile(file, out.toString(), false);
+            jda.getGuildById(336876056265097237L).getTextChannelById(379255301561516032L).sendFile(file.toFile(), new MessageBuilder().append("Server Information for ").append(jda.getSelfUser().getAsMention()).build()).queue();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
